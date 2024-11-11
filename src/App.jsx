c7d72f4130178f44a74f771bf9c3474a836c09f9
akramgadir <akram.gadir@gmail.com>
2024-11-11 02:58:19 +0000

    bug fixing for eas privacy and tos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import { BrowserRouter } from "react-router-dom"
-import {About, Contact, Experience, Tech, Feedbacks, Hero, Navbar, Works, StarsCanvas} from './components'
-import PrivacyPolicy from "./components/PrivacyPolicy"
-import TermsOfService from "./components/TermsOfService"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Contact, Experience, Tech, Feedbacks, Hero, Navbar, Works, StarsCanvas } from './components';
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import TermsOfService from "./components/TermsOfService";
 
 const App = () => {
-
   return (
-<BrowserRouter>
-<div className ='relative z-0 bg-primary'>
-  <div className = 'bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-<Navbar/>
-<Hero/>
-  </div>
-  <About />
-  <Experience />
-  <Tech />
-  <Works />
-  <Feedbacks />
-  <div className="relative z-0">
-    <Contact />
-    <StarsCanvas />
-  </div>
-</div>
-<Route path="/easprivacypolicy" element={<PrivacyPolicy />} />
-<Route path="/eastermsofservice" element={<TermsOfService />} />
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        {/* Main Website */}
+        <Route
+          path="/"
+          element={
+            <div className="relative z-0 bg-primary">
+              <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                <Navbar />
+                <Hero />
+              </div>
+              <About />
+              <Experience />
+              <Tech />
+              <Works />
+              <Feedbacks />
+              <div className="relative z-0">
+                <Contact />
+                <StarsCanvas />
+              </div>
+            </div>
+          }
+        />
+        {/* Privacy Policy Route */}
+        <Route path="/easprivacypolicy" element={<PrivacyPolicy />} />
+        <Route path="/eastermsofservice" element={<TermsOfService />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
